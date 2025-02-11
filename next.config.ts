12580366964fbfbbf95e55aa8f4c6356342239b2
Dara Gallopin <daragallopin@gmail.com>
2025-02-11 13:54:36 +0000

    config fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from 'next'
+import { NextConfig } from 'next'
 
-const config: Config = {
+const config: NextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
